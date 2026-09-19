8d1f449e1e309146bce625d9239093d8583e4370
John Chadwick <john@jchw.dev>
2026-09-19 00:23:49 -0400

    Add useful tools into flake

--- flake.nix
+++ flake.nix
@@ -7,26 +7,38 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      buildPkgs = [
+        pkgs.gnumake
+        pkgs.patch
+        pkgs.python3
+        pkgs.ruff
+        pkgs.ty
+        pkgs.wineWow64Packages.stable
+      ];
+      devPkgs = [
+        pkgs.llvmPackages_21.bintools
+        pkgs.llvmPackages_21.clang-tools
+        pkgs.radare2
+      ];
+      lintPkgs = [
+        pkgs.actionlint
+        pkgs.shellcheck
+        pkgs.zizmor
+      ];
     in {
       devShells.${system} = {
         default = pkgs.mkShell {
-          packages = with pkgs; [
-            python3
-            ruff
-            ty
-            gnumake
-            patch
-            wineWow64Packages.stable
-            llvmPackages_21.clang-tools
+          packages = builtins.concatLists [
+            buildPkgs
+            devPkgs
+            lintPkgs
           ];
         };
-
+        build = pkgs.mkShell {
+          packages = buildPkgs;
+        };
         lint = pkgs.mkShell {
-          packages = with pkgs; [
-            actionlint
-            shellcheck
-            zizmor
-          ];
+          packages = lintPkgs;
         };
       };
     };
